3d70564d170ec8f12e016b22bcc7f90ee8487a4f
chrissen <chrissen0814@gmail.com>
2025-06-09 15:04:55 +0900

    opt layout on praise ui

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -49,7 +49,7 @@ export default function LikeButton() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 20 }}
-                className="text-3xl font-bold text-white -mt-24"
+                className="text-2xl lg:text-3xl font-bold text-white lg:-mt-24"
               >
                 ありがとうございます
               </motion.p>
